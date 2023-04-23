84856c4bc80e3d613faf09099f6c8f41a7ac151b
Jonken <Noxxan@protonmail.com>
2023-04-23 12:26:10 +0200

    PizzaStore EF inMermory done

--- PizzaStoreEF/Program.cs
+++ PizzaStoreEF/Program.cs
@@ -18,5 +18,34 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
 });
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
-
+app.MapPost("/pizzas", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizzas/{pizza.Id}", pizza);
+});
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+app.MapPut("/pizzas/{id}", async (PizzaDb db, int id, Pizza updatepizza) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza == null)
+    {
+        return Results.NotFound();
+    }
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/pizzas/{id}", async (PizzaDb db, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza == null)
+    {
+        return Results.NotFound();
+    }
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
 app.Run();
